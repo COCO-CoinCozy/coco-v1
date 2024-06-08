1be4acaffec7aa6f4fa7c5b5bbcd697fdbe5b315
w0nnie <kjw55523@gmail.com>
2024-06-08 15:25:26 +0900

    init

---
+++ src/main/java/com/danghouse/cocov1/CocoV1Application.java
@@ -0,0 +1,13 @@
+package com.danghouse.cocov1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocoV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocoV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/danghouse/cocov1/CocoV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danghouse.cocov1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocoV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
